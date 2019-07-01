7c8c4c46b4dfee1da90a206b64703340be34fb85
kosgrz <grzegorz.kosik94@gmail.com>
2019-07-01 22:41:13 +0200

    adjust to more generalized interface

--- webapp/src/plugin.jsx
+++ webapp/src/plugin.jsx
@@ -3,6 +3,6 @@ import LatexBlock from 'latex_block';
 export default class LatexPlugin {
     // eslint-disable-next-line no-unused-vars
     initialize(registry) {
-        registry.registerLatexPlugin(LatexBlock);
+        registry.registerCodeBlockComponent(LatexBlock, ['tex', 'latex']);
     }
 }
