e78320eba191bc5a2f030227297bcd65f7537374
kosgrz <grzegorz.kosik94@gmail.com>
2019-08-07 00:01:48 +0200

    switch order of arguments in registerCodeBlockComponent method

--- webapp/src/plugin.jsx
+++ webapp/src/plugin.jsx
@@ -3,6 +3,6 @@ import LatexBlock from 'latex_block';
 export default class LatexPlugin {
     // eslint-disable-next-line no-unused-vars
     initialize(registry) {
-        registry.registerCodeBlockComponent(LatexBlock, ['tex', 'latex']);
+        registry.registerCodeBlockComponent(['tex', 'latex'], LatexBlock);
     }
 }
